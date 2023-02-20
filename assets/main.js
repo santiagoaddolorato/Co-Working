7f87cba3e233cb3d6c6016aa9ce29e7776a0d20c
Santiago Addolorato <addoloratosantiago@gmail.com>
2023-02-20 12:00:00 -0300

    trabajo terminado

--- assets/main.js
+++ assets/main.js
@@ -1,81 +1,97 @@
-const cardContainer = document.getElementById(`cards-container`);
-const form = document.getElementById(`form`);
+const cardsContainer = document.getElementById("cards-container");
+const form = document.getElementById("form");
 
 let agenda = JSON.parse(localStorage.getItem("agenda")) || [];
 
-const sabeLocalStorage = () => {
-    localStorage.setItem(`agenda`, JSON.stringify(agenda));
+const saveLocalStorage = () => {
+  localStorage.setItem("agenda", JSON.stringify(agenda));
 };
 
+// Nos va a devovler la agenda con su estado actual sumando el nuevo turno que carguemos.
+// Vamos a usar las funciones formatDate, getRadioValue y getCheckedOptiosn que estan en el archivito utils
+
 const saveDate = () => {
-    agenda= [
-        ...agenda,
-        {
-            id: agenda.length + 1,
-            name: nameInput.value,
-            surename: surenameInput.value,
-            phone: phoneInput.value,
-            email: emailInput.value,
-            date:  formatDate(dateInput.value),
-            time: timeInput.value,
-            quantity: getRadioValue(radioInput),
-            extras: getCheckedOptions(checkBoxInputs),
-            about: aboutInput.value
-        }
-    ]
-}
+  agenda = [
+    ...agenda,
+    {
+      id: agenda.length + 1,
+      name: nameInput.value,
+      surname: surnameInput.value,
+      phone: phoneInput.value,
+      email: emailInput.value,
+      date: formatDate(dateInput.value),
+      time: hourInput.value,
+      quantity: getRadioValue(radioInputs),
+      extras: getCheckedOptions(checkboxInputs),
+      about: aboutInput.value,
+    },
+  ];
+};
 
-const renderTurn = (turn)=> {
-    const {id,name, surename, phone, email, date,time, quantity,  extras } = turn;
-    return
+// Funcion para crear una card de turno.
+// recibe un turno y devuelve una card html con los valores correspondientes.
 
-`
-<div class="card ${setCardBackground(quantity)} ">
+const renderTurn = (turn) => {
+  const { id, name, surname, phone, email, date, time, quantity, extras } =
+    turn;
+  console.log(extras);
+  return `
+  <div class="card ${setCardBackground(quantity)}">
         <div class="card__left">
-          <h2 class="card__title">Orden: N1 ${id} - ${name}${surename}</h2>
+          <h2 class="card__title">Orden: N°${id} - ${name} ${surname}</h2>
           <p class="card__qty">${quantity}</p>
-          <p class="card__extras">Extra: ${extras}</p>
+          <p class="card__extras">Extra: ${
+            extras && extras.length
+              ? extras.join(", ")
+              : "No requiere herramientas extras."
+          }</p>
           <div class="tags">
-            <span class="card__hour ${setTimeBackground (quantity)}">${time} HS</span>
-            <span class="card__date  ${setTimeBackground(quantity)}"> ${date}</span>
+            <span class="card__hour ${setTimeBackground(
+              quantity
+            )}">${time} HS</span>
+            <span class="card__date ${setDateBackground(
+              quantity
+            )}">${date}</span>
           </div>
         </div>
         <div class="card__right">
         <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=${email}" target="_blank"><i class="fa-solid fa-envelope"></i></a>
         <a href="https://api.whatsapp.com/send?phone=+549${phone}&text=Boenas" target="_blank"><i class="fa-solid fa-phone-flip"></i></a>
-          <img src=${setCardImg(quantity)}  class="card__img ${setCardImgClass(
-            quantity
-          )}" alt="" />
+          <img src=${setCardImg(quantity)} class="card__img ${setCardImgClass(
+    quantity
+  )}" alt="" />
         </div>
       </div>
-      `;};
+    `;
+};
 
-      const renderAgenda= () => {
-        cardContainer.innerHTML = agenda.maps((turn) => renderTurn(turn)).join("");
-      };
+// Función nos va a servier para renderizar la agenda, misma logica que aplicamos en el todo list
+const renderAgenda = () => {
+  cardsContainer.innerHTML = agenda.map((turn) => renderTurn(turn)).join("");
+};
 
-      const submitForm = (e) => {
-        e.preventDefault();
-        if(isValidForm()){
-            saveDate();
-            alert("El turno se ah cargado correctamente")
-            console.log(agenda);
-            form.reset();
-            saveLocalStorage();
-            renderAgenda();
-            setDateIntervals();
+// Funcion va a recibir el evento submit del formulario
 
-        }
+const submitForm = (e) => {
+  e.preventDefault();
+  if (isValidForm()) {
+    saveDate();
+    alert("El turno se ha cargado correctamente");
+    console.log(agenda);
+    form.reset();
+    saveLocalStorage();
+    renderAgenda();
+    setDateIntervals();
+  }
+};
 
-      };
+const init = () => {
+  // Renderizamos la agenda en caso de que ya haya algo en el localstorage
+  renderAgenda();
 
-      const init = () => {
-        renderAgenda();
+  window.addEventListener("DOMContentLoaded", setDateIntervals);
+  form.addEventListener("submit", submitForm);
+};
 
-        window.addEventListener("DOMContentLoaded", setDateIntervals);
-        form.addEventListener("submit", submitForm);
-      }
-
-      init();
-
+init();
       
--- assets/utils.js
+++ assets/utils.js
@@ -1,78 +1,166 @@
+// Manejo de fechas
+
+// Función para obtener el día siguiente al día actual
 const getNextDay = () => {
-    let tomorrow = new Date ();
-    tomorrow.setDate(tomorrow.getDate() +1);
-};
-
-
-const padTo2Digits = (num) =>{
-    return num.toString().padStart(2,"0");
-
-};
-
-const getTomorrowDate = () => {
-    let year = getNextDay().getFullYear();
-    let day = getNextDay().getDate();
-    let month = getNextDay().getMonth() + 1;
-
-    return `${year}-${padTo2Digits(month)}- ${padTo2Digits(day)}`;
-};
-
- const getDateInvervals = () => {
-    dateInput.value = getTomorrowDate();
-    dateInput.min = getTomorrowDate();
-    dateInput.max = getNextDay().getFullYear() + "-12-31"
- }
-
- const isEmpty = (value)=> {
-    !value.length;
+   let tomorrow = new Date();
+   tomorrow.setDate(tomorrow.getDate() + 1);
+   return tomorrow;
  };
-
- const isEmailValid = (email)=> {
-    const re =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-    return re.test(email);
+ 
+ // Función para formatear el número de fecha.
+ // 25/01/1999
+ // DD/MM/AAAA
+ const padTo2Digits = (num) => {
+   return num.toString().padStart(2, "0");
  };
-
- const isPhoneValid = (phone) =>{
-  const re =/^[0-9]{10}$/;
-  return re.test(phone);
+ 
+ // Función para que en base a una fecha, obtengamos año, día y
+ // mes y retornemos la fecha en el formato que necesitamos para guardarla.
+ 
+ const getTomorrowDate = () => {
+   let year = getNextDay().getFullYear();
+   let day = getNextDay().getDate();
+   let month = getNextDay().getMonth() + 1;
+ 
+   return `${year}-${padTo2Digits(month)}-${padTo2Digits(day)}`;
  };
-
+ 
+ // Función para setear el valor inicial del input de tipo date y el
+ // valor mínimo y máximo que puede tomar.
+ // 1. Seteamos el valor inicial en la fecha de mañana usando getTomorrowDate()
+ // 2. El mínimo será tambien el día de mañana, para no poder ir a una fecha anterior a esa
+ // 3. El maximo, como dijimos, esta seteado en el último día del año actual.
+ // Le sacamos el año a la fecha actual y lo concatenamos con "-12-31" para que nos tome la fecha del 31 de diciembre del año actual
+ 
+ const setDateIntervals = () => {
+   dateInput.value = getTomorrowDate();
+   dateInput.min = getTomorrowDate();
+   dateInput.max = getNextDay().getFullYear() + "-12-31";
+ };
+ 
+ // ----------------------------------------------------
+ // ------------ Checkeo de validaciones ---------------
+ // ----------------------------------------------------
+ 
+ const isEmpty = (value) => {
+   return !value.length;
+ };
+ 
+ const isEmailValid = (email) => {
+   const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+   return re.test(email);
+ };
+ 
+ const isPhoneValid = (phone) => {
+   const re = /^[0-9]{10}$/;
+   return re.test(phone);
+ };
+ // Checkeamos si la fecha dada por el input es valida.
  const isValidDate = (date) => {
-    const currentDate = new Date ();
-    const turnDate= new Date (date);
-
-    turnDate.setHours(currentDate.getHours());
-    turnDate.setMinutes(currentDate.getMinutes());
-    turnDate.setSeconds(currentDate.getSeconds());
-    turnDate.setDate(turnDate.getDate() +1);
-
-    return date.length && currentDate < turnDate;
+   const currentDate = new Date();
+   const turnDate = new Date(date);
+ 
+   turnDate.setHours(currentDate.getHours());
+   turnDate.setMinutes(currentDate.getMinutes());
+   turnDate.setSeconds(currentDate.getSeconds());
+   turnDate.setDate(turnDate.getDate() + 1);
+ 
+   //   console.log("Date =>", currentDate);
+   //   console.log("Date =>", currentDate < turnDate);
+   //   console.log("Datenew =>", turnDate);
+   //   console.log("Datenew =>", currentDate > turnDate);
+   return date.length && currentDate < turnDate;
  };
-
+ 
+ // manejo de errores
+ 
+ // funcion para mostrar error
  const showError = (input, message) => {
-    const formField = input.parentElement;
-    formField.classList.add("error");
-    const error = formField.querySelector("small");
-    error.textContent = message;
+   const formField = input.parentElement;
+   formField.classList.add("error");
+   const error = formField.querySelector("small");
+   // innerText
+   error.textContent = message;
  };
-
-
- const clearError =(input) => {
-    const formField = input.parentElement;
-    formField.classList.remove("error");
-    const error =formField.querySelector("small");
-    error.textContent= "";
+ 
+ // Funcion para borrar el error
+ const clearError = (input) => {
+   const formField = input.parentElement;
+   formField.classList.remove("error");
+   const error = formField.querySelector("small");
+   // innerText
+   error.textContent = "";
  };
-
- const getRadioValue = (input) => {
-    const checkedInput = [...inputs].find((input) => input.checked);
-    return checkedInput.value;
+ 
+ // utils para propiedad del objeto que va a la agenda
+ 
+ const getRadioValue = (inputs) => {
+   const checkedInput = [...inputs].find((input) => input.checked);
+   return checkedInput.value;
  };
-
- const getCheckedOptions =(input) => {
-    const checkedOptions = [...input].filter((input) => input.checked).map((opt)=> opt.value);
+ // Función para obtener los values de los inputs de tipo checkbox que esten seleccionados(extras)
+ // Recibe un HTMLCollection de inputs( que serán todos los de tipo CHECKBOX cuando la llamemos).
+ // Devuelve un array con los values de los elementos seleccionados.
+ const getCheckedOptions = (inputs) => {
+   console.log("HTMLCOLLECTION", inputs, typeof inputs);
+   console.log("ARRAY DE ELEMENTOS", [...inputs], typeof [...inputs]);
+ 
+   const checkedOptions = [...inputs]
+     .filter((input) => input.checked)
+     .map((opt) => opt.value);
+   return checkedOptions;
  };
-
+ 
+ // Funcion para formatear la fecha que nos devuelve el input date, para asi despues mostrar
+ 
+ // recibe "2022-08-25"
+ // splitDate ["2022", "08", "25"]
+ // ["25","08","2022"]
+ // splitDate = 25/08/2022
  const formatDate = (date) => {
-    const spliteDate = date.split ("-").reverse().join("/")
+   const splitDate = date.split("-").reverse().join("/");
+   return splitDate;
+ };
+ 
+ // Formato de las Cards
+ 
+ // Funcion que setea el color de fondo de la card
+ 
+ const setCardBackground = (quantity) => {
+   return quantity === "Más de 10"
+     ? "violet-card"
+     : quantity === "Entre 5 y 10"
+     ? "red-card"
+     : "black-card";
+ };
+ 
+ // Funcion para setear el color del span de horario segun la cantidad de personas
+ const setTimeBackground = (quantity) => {
+   return quantity === "Menos de 5" ? "red-card" : "black-card";
+ };
+ 
+ // Funcion para setear el color del span de fecha segun la cantidad de personas
+ 
+ const setDateBackground = (quantity) => {
+   return quantity === "Más de 10" ? "red-card" : "violet-card";
+ };
+ 
+ // Funcion para setear la imagen que acompáñar las cards segun la cantidad de personas
+ 
+ const setCardImg = (quantity) => {
+   return quantity === "Más de 10"
+     ? "./assets/img/server.png"
+     : quantity === "Entre 5 y 10"
+     ? "./assets/img/laptop.png"
+     : "./assets/img/lupa.png";
+ };
+ 
+ // Funcion para setear la clase de la imagen que acompañar a alas cards de cantidad de personas
+ 
+ const setCardImgClass = (quantity) => {
+   return quantity === "Más de 10"
+     ? "server-img"
+     : quantity === "Entre 5 y 10"
+     ? "laptop-img"
+     : "lupa-img";
  };
